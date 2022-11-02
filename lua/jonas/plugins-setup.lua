da133b46a87edc17409aafacbe7f398ad9fe42d1
Sokke91 <jonas.angel.ja@gmail.com>
2022-11-02 22:17:40 +0100

    add git plugin

--- lua/jonas/plugins-setup.lua
+++ lua/jonas/plugins-setup.lua
@@ -79,11 +79,10 @@ return packer.startup(function(use)
 			require("nvim-treesitter.install").update({ with_sync = true })
 		end,
 	})
-
 	-- auto closing
 	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
 	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
-
+	use({ "dinhhuy258/git.nvim" })
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
